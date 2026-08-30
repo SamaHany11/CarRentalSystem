fb28c69fd5a05f3f66108a9790c7d4844b12af39
Sama Hany <samafadol@gmail.com>
2026-08-30 13:15:12 +0300

    Added customer and exceptions classes

---
+++ src/main/java/org/FirstTest/CarNotAvailableException.java
@@ -0,0 +1,7 @@
+package org.FirstTest;
+
+public class CarNotAvailableException extends Exception {
+    public CarNotAvailableException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/org/FirstTest/DuplicateActiveRentalException.java
@@ -0,0 +1,7 @@
+package org.FirstTest;
+
+public class DuplicateActiveRentalException extends Exception {
+    public DuplicateActiveRentalException(String message){
+        super(message);
+    }
+}
